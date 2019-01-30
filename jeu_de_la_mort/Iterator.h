c6b42aeba7f8eda593ff1ed7e25e4a747995de3b
OlivierArgentieri <olivierargentieri@gmail.com>
2019-01-30 12:01:49 +0100

    [OAI] add class

---
+++ jeu_de_la_mort/Iterator.h
@@ -0,0 +1,102 @@
+#pragma once
+#include "Node.h"
+
+template<typename T>
+class MyNewList;
+
+template<typename T>
+class Iterator
+{
+	template<typename T> friend class MyNewList;
+	template<typename T> friend class Node;
+public:
+
+	Iterator()
+	{
+
+	}
+
+	Iterator(Node<T> *position, MyNewList<T>* list)
+	{
+		element = position;
+		m_list = list;
+	}
+
+	~Iterator()
+	{
+
+	}
+
+	Iterator<T>& operator++(int i)
+	{
+		int j = 0;
+		while (j != i && element != nullptr)
+		{
+			element = element->Next;
+			j++;
+		}
+		if (j == i)
+			return *this;
+		else
+			throw std::out_of_range("ERROR");
+	}
+
+	Iterator<T>& operator--(int i)
+	{
+		int j = 0;
+		while (j != i && element != nullptr)
+		{
+			element = element->Previous;
+			j++;
+		}
+		if (j == i)
+			return *this;
+		else
+			throw std::out_of_range("ERROR");
+	}
+
+	bool operator==(Iterator<T> FirstElement)
+	{
+		return FirstElement.element == element;
+	}
+
+	bool operator!=(Iterator<T> FirstElement)
+	{
+		return FirstElement.element != element;
+	}
+
+	Iterator<T>& operator++()
+	{
+		if (element != nullptr)
+		{
+			element = element->Next;
+			return *this;
+		}
+		else
+			throw std::out_of_range("ERROR");
+	}
+	Iterator<T>& operator--()
+	{
+		if (element != nullptr)
+		{
+			element = element->Previous;
+			return *this;
+		}
+		else if (element == m_list->End().element)
+		{
+			element = m_list->Queue;
+			return *this;
+		}
+		else
+			throw std::out_of_range("ERROR");
+	}
+
+	T& operator*()
+	{
+		return element->value;
+	}
+
+private:
+	Node<T> *element;
+	MyNewList<T> *m_list;
+};
---
+++ jeu_de_la_mort/MyNewList.h
@@ -0,0 +1,238 @@
+#pragma once
+#include "Node.h"
+#include "Iterator.h"
+
+template<typename T>
+class MyNewList
+{
+	template <typename T> friend class Iterator;
+	template<typename T> friend class Node;
+public:
+	MyNewList()//Initialize list
+	{
+		Header = nullptr;
+		Queue = nullptr;
+		size = 0;
+	}
+	~MyNewList()
+	{
+
+	}
+	void PushFront(T NewElement)//Add new element at front of the list
+	{
+		AddAt(NewElement, this->Begin());
+	}
+	void PushBack(T NewElement)//Add new element at back of the list
+	{
+		AddAt(NewElement, this->End());
+	}
+	void AddAt(T NewElement, Iterator<T> Position)//Add new element at a position i
+	{
+		Node<T> *newNode = new Node<T>();
+		newNode->value = NewElement;
+		if (Position.element == nullptr && size == 0)
+		{
+			Header = newNode;
+			Queue = newNode;
+		}
+		else if (Position.element == nullptr)
+		{
+			Queue->Next = newNode;
+			newNode->Previous = Queue;
+			Queue = newNode;
+		}
+		else if (Position.element->Previous == nullptr)
+		{
+			newNode->Next = Header;
+			Header->Previous = newNode;
+			Header = newNode;
+		}
+		else
+		{
+			newNode->Previous = Position.element->Previous;
+			newNode->Next = Position.element;
+			Position.element->Previous->Next = newNode;
+			Position.element->Previous = newNode;
+		}
+		size++;
+	}
+	void EraseFront()//Erase first element of the list
+	{
+		EraseAt(this->Begin());
+	}
+	void EraseBack()//Erase last element of the list
+	{
+		EraseAt(this->Begin().operator++(size - 1));
+	}
+
+	Iterator<T> EraseAt(Iterator<T> Position)//Erase at a position i
+	{
+		if (Position.element != nullptr)
+		{
+			Iterator<T> ToReturn;
+			if (Header != Queue)
+			{
+				if (Position.element->Next == nullptr)
+				{
+					Queue = Queue->Previous;
+					Queue->Next = nullptr;
+					ToReturn = End();
+					delete(Position.element);
+				}
+				else if (Position.element->Previous == nullptr)
+				{
+					Header = Header->Next;
+					Header->Previous = nullptr;
+					ToReturn = Begin();
+					delete(Position.element);
+				}
+				else
+				{
+					Position.element->Next->Previous = Position.element->Previous;
+					Position.element->Previous->Next = Position.element->Next;
+					ToReturn = Position.operator++();
+					delete(Position.element);
+				}
+			}
+			else
+			{
+				delete(Header);
+				ToReturn = End();
+			}
+			size--;
+			return ToReturn;
+		}
+		else
+			throw std::out_of_range("ERROR");
+	}
+
+	void Clear()//Erase all elements
+	{
+		while (size != 0)
+		{
+			EraseFront();
+		}
+		Header = nullptr;
+		Queue = nullptr;
+	}
+	void Remove(T i)//Remove an element i of the list
+	{
+		int j = 0;
+		Iterator<T> it = Begin();
+		while (j != size)
+		{
+			if (it.element->HaveValue() == i && it.element->Previous == nullptr)
+			{
+				EraseFront();
+				it = Begin();
+				j--;
+			}
+			else if (it.element->HaveValue() == i && it.element->Next == nullptr)
+			{
+				EraseBack();
+				it = End();
+				j--;
+			}
+			else if (it.element->HaveValue() == i)
+			{
+				EraseAt(this->Begin().operator++(j));
+				j--;
+				it = this->Begin().operator++(j + 1);
+			}
+			else
+				it.operator++();
+			j++;
+		}
+	}
+
+	Iterator<T> Begin()//Get first element of the list
+	{
+		return Iterator<T>(Header, this);
+	}
+
+	Iterator<T> End()//Get last element of the list
+	{
+		return Iterator<T>(nullptr, this);
+	}
+
+	int Size()//Get list size
+	{
+		return size;
+	}
+
+	Iterator<T> *At(int i)//Search a node at a position i
+	{
+		int j = 0;
+		Iterator<T> *it = new Iterator<T>(Header, this);;
+		while (j != i)
+		{
+			if (it->element != nullptr)
+			{
+				++it;
+				j++;
+			}
+			else
+				throw std::out_of_range("ERROR");
+		}
+		return it;
+	}
+
+private:
+
+	Node<T> *Header;
+	Node<T> *Queue;
+	int size;
+
+	//void SwapNodes(Iterator<T> *First, Iterator<T> *Second)//Swap Nodes
+	//{
+	//	Iterator<T> *swapnode = new Iterator<T>();
+	//	if (First->element != nullptr && Second->element != nullptr)
+	//	{
+	//		InitializationNewNode(swapnode, First);
+	//		ChangeNode(First, Second);
+	//		ChangeNode(Second, swapnode);
+	//		delete(swapnode);
+	//	}
+	//	else
+	//		throw std::out_of_range("ERROR");
+	//}
+
+	//void InitializationNewNode(Iterator<T> *NewNode, Iterator<T> *BaseNode)//Initialization for placing a new node
+	//{
+	//	NewNode->element->Next = BaseNode->element->Next;
+	//	NewNode->element->Previous = BaseNode->element->Previous;
+	//	NewNode->element->value = BaseNode->element->value;
+	//}
+
+	//void ChangeNode(Iterator<T> *First, Iterator<T> *Second)//Affect second node to first node
+	//{
+	//	if (First->element->Next == Second->element)
+	//	{
+	//		First->element->Next = Second->element->Next;
+	//		First->element->Previous = Second->element;
+	//	}
+	//	else if (First->element->Previous = Second->element)
+	//	{
+	//		First->element->Previous = Second->element->Previous;
+	//		First->element->Next = First->element;
+	//	}
+	//	else
+	//	{
+	//		First->element->Next = Second->element->Next;
+	//		First->element->Previous = Second->element->Previous;
+	//	}
+	//	Verification(First, Second);
+	//}
+
+	//void Verification(Iterator<T> *First, Iterator<T> *Second)
+	//{
+	//	if (Second->element->Next != nullptr)
+	//		Second->element->Next->Previous = First->element;
+	//	if (Second->element->Previous != nullptr)
+	//		Second->element->Previous->Next = First->element;
+	//	if (First->element->Next == nullptr)
+	//		Queue = First->element;
+	//	if (First->element->Previous == nullptr)
+	//		Header = First->element;
+	//}
+};
