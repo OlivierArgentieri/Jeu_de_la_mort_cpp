830f4f78b185828b5ee30e2706668fe1955c5c23
OlivierArgentieri <30509073+OlivierArgentieri@users.noreply.github.com>
2019-02-05 16:54:09 +0100

    [AOI] Add color enum

---
+++ jeu_de_la_mort/Color.h
@@ -0,0 +1,13 @@
+#pragma once
+
+enum color
+{
+	RED = 31,
+	GREEN = 32,
+	YELLOW = 33,
+	BLUE = 34,
+	MAGENTA = 35,
+	CYAN = 36,
+	WHITE = 37,
+	RESET = 0
+};
--- jeu_de_la_mort/Sprite.h
+++ jeu_de_la_mort/Sprite.h
@@ -1,5 +1,15 @@
 #pragma once
+#include "Color.h"
+
 class Sprite
 {
-	
+private:
+	color m_color_;
+	char m_char_;
+
+public:
+	void SetColor(color _cColor);
+
+	void Display();
+
 };
