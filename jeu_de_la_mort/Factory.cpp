8d342f31a1c4b8f4304500c3f200120cece31156
OlivierArgentieri <30509073+OlivierArgentieri@users.noreply.github.com>
2019-02-06 12:02:26 +0100

    [OAI] Add facotry and method 2/2

--- jeu_de_la_mort/Factory.cpp
+++ jeu_de_la_mort/Factory.cpp
@@ -1,10 +1,11 @@
 #include "pch.h"
 #include "Factory.h"
 #include <cstdlib>
-#include "Human.h"
 #include "Civil.h"
 #include "Doctor.h"
 #include "Military.h"
+#include "ZombieRandomMove.h"
+#include "ZombieExplode.h"
 
 human Factory::GetRandomHumanEnum()
 {
@@ -38,4 +39,37 @@ void Factory::InitiateRandomHuman(Vector2 _v2Position)
 
 	human h = Factory::GetRandomHumanEnum();
 	Factory::CreateHuman(h, _v2Position);
+}
+
+
+zombie Factory::GetRandomZombieEnum()
+{
+	return zombie(rand() % 2);
+}
+
+
+void Factory::CreateZombie(zombie _eZombie, Vector2 _v2Position)
+{
+	switch (_eZombie)
+	{
+	case ZOMBIE_RANDOM_MOVE:
+		new ZombieRandomMove(_v2Position);
+		break;
+
+	case ZOMBIE_EXPLODE:
+		new ZombieExplode(_v2Position);
+		break;
+
+	default:
+		break;
+	}
+}
+
+void Factory::InitiateRandomZombie(Vector2 _v2Position)
+{
+	if (_v2Position == -1)
+		return;
+
+	zombie z = Factory::GetRandomZombieEnum();
+	Factory::CreateZombie(z, _v2Position);
 }
---
+++ jeu_de_la_mort/ZombieEnum.h
@@ -0,0 +1,6 @@
+#pragma once
+enum zombie
+{
+	ZOMBIE_EXPLODE,
+	ZOMBIE_RANDOM_MOVE
+};
